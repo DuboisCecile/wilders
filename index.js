9ddc6f1dd854b3f385217dd94f8204ef8fc3c6be
Cécile Dubois <duboiscecilepro@gmail.com>
2021-03-17 10:09:32 +0100

    Sending exercise npm answer

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue.bgGreen('Nelson'));
+console.log(chalk.yellow.bgBlue('Benoît'));
+console.log(chalk.red.bgWhite('Florian'));
+console.log(chalk.magentaBright('Solène'));
